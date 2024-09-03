7aa6460b1260c54def4673ac28724bdad4f93169
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-09-03 20:25:42 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,6 @@
 package com.driver;
 
-public class Main {
+public static class Main {
     public static void main(String[] args) {
         B obj = new B();
         System.out.println(obj.meth());
