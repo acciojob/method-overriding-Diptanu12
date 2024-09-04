19ce6690c8e7a3cfa5ce77d79843309c897806db
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-09-04 22:13:36 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,10 +1,14 @@
 package com.driver.test;
 
-
+import com.driver.B;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestCases {
-
+    @Test
+    public void testMeth() {
+        B obj = new B();
+        String result = obj.meth();
+        assertEquals("Method is overridden in Extended class B", result);
     }
 }
