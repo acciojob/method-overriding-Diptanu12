31948f47483a7208c4cedc5a59fc52eef048683d
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-09-04 22:06:00 +0530

    Update B.java

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,6 +1,6 @@
 package com.driver;
-package com.driver.A.java;
-public static class B extends A {
+
+public class B extends A {
     @Override
     public String meth() {
         return "Method is overridden in Extended class B";
